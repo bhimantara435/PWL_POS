f1e8d42053247e50ed87b309f08b63e60b38252a
BHIMANTARA435 <bhimantara.bhima21@gmail.com>
2025-03-14 20:31:29 +0700

    praktikum 4, - J3

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\levelController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +17,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+
+
 });
+
+Route::get('/level',[levelController::class, 'index']);
