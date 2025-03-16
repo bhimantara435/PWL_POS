fcebce3184908b2d71b2933a52404b91b5fd00f2
BHIMANTARA435 <bhimantara.bhima21@gmail.com>
2025-03-16 23:06:07 +0700

    prak 2.5 J-4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,19 +7,23 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
-{
-    public function index()
+{    public function index()
     {
+        $user = UserModel::create([
+            'username' => 'manager11',
+            'nama' => 'Manager11',
+            'password' => Hash::make('12345'),
+            'level_id' => 2,
+        ]);
 
-        $user = UserModel::firstOrNew(
-                [
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
-                'password' => Hash :: make('12345'),
-                'level_id' => 2
-                ],
-                );
-                $user->save();
+        $user->username = 'manager12';
+        $user->save();
+
+        $user->wasChanged();
+        $user->wasChanged('username');
+        $user->wasChanged(['username', 'level_id']);
+        $user->wasChanged('nama');
+        dd($user->wasChanged(['nama', 'username']));
         return view('user', ['data' => $user]);
     }
 }
