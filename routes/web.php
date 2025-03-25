407f1caa87126bb87806a407a9329ca2f0a1a8d7
bhimantara435 <bhimantara.bhima21@gmail.com>
2025-03-25 19:35:22 +0700

    J5 - PRAK 3

--- routes/web.php
+++ routes/web.php
@@ -17,21 +17,18 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-
-
-});
-
-Route::get('/level',[levelController::class, 'index']);
-Route::get('/kategori',[kategoriController::class, 'index']);
-Route::get('/user',[UserController::class, 'index']);
-Route::get('/user/tambah',[UserController::class, 'tambah']) ;
-Route::post('/user/tambah_simpan',[UserController::class, 'tambah_simpan']) ;
-Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
-Route::put('/user/ubah_simpan/{id}', [UserController :: class, 'ubah_simpan']);
-Route::get('/user/hapus/{id}',[UserController::class, 'hapus']);
 Route::get('/', [WelcomeController::class, 'index']);
 
 
+Route::group(['prefix' => 'user'], function () {
+    Route::get('/', [UserController::class, 'index']);
+    Route::post('/list', [UserController::class, 'list']);
+    Route::get('/create', [UserController::class, 'create']);
+    Route::post('/', [UserController::class, 'store']);
+    Route::get('/{id}', [UserController::class, 'show']);
+    Route::get('/{id}/edit', [UserController::class, 'edit']);
+    Route::put('/{id}', [UserController::class, 'update']);
+    Route::delete('/{id}', [UserController::class, 'destroy']);
+});
+Route::get('/level',[levelController::class, 'index']);
+Route::get('/kategori',[kategoriController::class, 'index']);
